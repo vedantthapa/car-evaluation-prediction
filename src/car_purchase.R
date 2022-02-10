039c30ddbb779a00cce0941577358e3bf60bfcc9
vedantthapa <thapavedant@gmail.com>
2022-02-10 10:41:35 -0400

    Adds dt model

--- src/car_purchase.R
+++ src/car_purchase.R
@@ -1,19 +1,52 @@
 library(dplyr)
 library(ggplot2)
 library(GGally)
+library(caret)
+library(pROC)
 
-
-data = read.csv("~/Downloads/car-purchase/assets/data/car-data.csv")
+data = read.csv("~/Downloads/car-purchase/assets/data/car_data.csv")
 head(data)
 
-unique(data$shouldBuy)
+set.seed(42)
+outer_train.idx = createDataPartition(
+  data$shouldBuy,
+  times = 1,
+  p = 0.95,
+  list=FALSE
+)
+outer_train = data[outer_train.idx,]
 
-data %>%
-  group_by(shouldBuy) %>%
-  summarise(n = n()) %>%
-  mutate(freq = n / sum(n))
+set.seed(42)
+inner_train.idx <- createDataPartition(
+  outer_train$shouldBuy,
+  times = 1,
+  p = 0.85,
+  list=FALSE
+)
+inner_train = outer_train[inner_train.idx,]
+valid = outer_train[-inner_train.idx,]
 
-rapply(data,function(x)length(unique(x)))
+set.seed(42)
+kfold = trainControl(method="cv",number=10)
 
-head(data)
-dim(data)
+set.seed(42)
+dt <- train(shouldBuy ~ .,
+            data = inner_train,
+            method = "rpart",
+            parms = list(split="information"),
+            trControl = kfold)
+
+inner_train.pred = predict(dt, inner_train)
+valid.pred = predict(dt, valid)
+
+confusionMatrix(data = valid.pred,
+                reference = as.factor(valid$shouldBuy))
+
+inner_train.prob = predict(dt, inner_train, prob = TRUE)
+valid.prob = predict(dt, valid, prob = TRUE)
+
+
+valid.roc = multiclass.roc(valid$shouldBuy, as.numeric(valid.prob))
+auc(valid.roc)
+
+
