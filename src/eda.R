48bd7f2c27b18f55d17875911d36e74acff327e3
vedantthapa <thapavedant@gmail.com>
2022-02-12 23:00:33 -0400

    Updates script to add relationship code

--- src/eda.R
+++ src/eda.R
@@ -3,20 +3,23 @@ library(naniar)
 library(ggplot2)
 library(GGally)
 library(patchwork)
+library(viridis)
+library(hrbrthemes)
 
-inner_train = read.csv("~/Downloads/car-purchase/assets/data/trainCar.csv")
-valid = read.csv("~/Downloads/car-purchase/assets/data/validCar.csv")
-dim(inner_train)
+
+outer_train = read.csv("~/Downloads/car-purchase/assets/data/outer_train.csv")
+
+dim(outer_train)
 
 # missing val plot
-inner_train %>%
+outer_train %>%
   vis_miss()
 
 # checking for duplicates
-inner_train[duplicated(inner_train) | duplicated(inner_train, fromLast = TRUE), ]
+outer_train[duplicated(outer_train) | duplicated(outer_train, fromLast = TRUE), ]
 
 # individual distributions
-p1 = inner_train %>% 
+p1 = outer_train %>% 
   count(price) %>% 
   ggplot(aes(n, price, fill = as.factor(n))) +
   geom_col() +
@@ -24,7 +27,7 @@ p1 = inner_train %>%
   theme(legend.position = "none") +
   labs(x = "", y = "", title = "Price")
 
-p2 = inner_train %>% 
+p2 = outer_train %>% 
   count(maintenance) %>% 
   ggplot(aes(n, maintenance, fill = as.factor(n))) +
   geom_col() +
@@ -32,7 +35,7 @@ p2 = inner_train %>%
   theme(legend.position = "none") +
   labs(x = "", y = "", title = "Maintenance")
 
-p3 = inner_train %>% 
+p3 = outer_train %>% 
   count(doors) %>% 
   ggplot(aes(n, doors, fill = as.factor(n))) +
   geom_col() +
@@ -40,7 +43,7 @@ p3 = inner_train %>%
   theme(legend.position = "none") +
   labs(x = "", y = "", title = "Doors")
 
-p4 = inner_train %>% 
+p4 = outer_train %>% 
   count(safety) %>% 
   ggplot(aes(n, safety, fill = as.factor(n))) +
   geom_col() +
@@ -48,7 +51,7 @@ p4 = inner_train %>%
   theme(legend.position = "none") +
   labs(x = "", y = "", title = "Safety")
 
-p5 = inner_train %>% 
+p5 = outer_train %>% 
   count(storage) %>% 
   ggplot(aes(n, storage, fill = as.factor(n))) +
   geom_col() +
@@ -56,15 +59,15 @@ p5 = inner_train %>%
   theme(legend.position = "none") +
   labs(x = "", y = "", title = "Storage")
 
-p6 = inner_train %>% 
-  count(shouldBuy) %>% 
-  ggplot(aes(shouldBuy, n, fill = as.factor(n))) +
+p6 = outer_train %>% 
+  count(seats) %>% 
+  ggplot(aes(n, seats, fill = as.factor(n))) +
   geom_col() +
-  theme_classic() +
+  theme_minimal() +
   theme(legend.position = "none") +
-  labs(x = "shouldBuy", y = "", title = "Target")
+  labs(x = "", y = "", title = "Seats")
 
-p6 = inner_train %>% 
+p7 = outer_train %>% 
   count(shouldBuy) %>% 
   mutate(pct = prop.table(n)) %>% 
   ggplot(aes(x = shouldBuy, y = pct, fill = as.factor(n), label = scales::percent(pct))) + 
@@ -77,8 +80,67 @@ p6 = inner_train %>%
   theme(legend.position = "none") +
   labs(x = "shouldBuy", y = "", title = "Target")
 
-((p1 + p2) / (p3 + p4) / p5) | p6
-
-##########################################################
+((p1 + p2) / (p3 + p4) / (p5 + p6)) | p7
 
 
+# relationship with target
+p1 =  outer_train %>%
+  group_by(price, shouldBuy) %>%
+  summarise(n=n()) %>%
+  ggplot(aes(fill = shouldBuy, y = price, x = n)) + 
+  geom_bar(position = "fill", stat = "identity",  colour = "black", alpha = 0.75) + 
+  scale_fill_viridis(discrete = T) +
+  scale_x_continuous(labels = scales::percent) +
+  labs(x = "", y = "",title = "Price") + 
+  theme(legend.position="none")
+
+p2 =  outer_train %>%
+  group_by(maintenance, shouldBuy) %>%
+  summarise(n=n()) %>%
+  ggplot(aes(fill = shouldBuy, y = maintenance, x = n)) + 
+  geom_bar(position = "fill", stat = "identity",  colour = "black", alpha = 0.75) + 
+  scale_fill_viridis(discrete = T) +
+  scale_x_continuous(labels = scales::percent) +
+  labs(x = "", y = "",title = "Maintenance") + 
+  theme(legend.position="none")
+
+p3 =  outer_train %>%
+  group_by(seats, shouldBuy) %>%
+  summarise(n=n()) %>%
+  ggplot(aes(fill = shouldBuy, y = seats, x = n)) + 
+  geom_bar(position = "fill", stat = "identity",  colour = "black", alpha = 0.75) + 
+  scale_fill_viridis(discrete = T) +
+  scale_x_continuous(labels = scales::percent) +
+  labs(x = "", y = "",title = "Doors") +
+  theme(legend.position="none")
+
+p4 =  outer_train %>%
+  group_by(safety, shouldBuy) %>%
+  summarise(n=n()) %>%
+  ggplot(aes(fill = shouldBuy, y = safety, x = n)) + 
+  geom_bar(position = "fill", stat = "identity",  colour = "black", alpha = 0.75) + 
+  scale_fill_viridis(discrete = T) +
+  scale_x_continuous(labels = scales::percent) +
+  labs(x = "", y = "",title = "Safety") 
+
+p5 =  outer_train %>%
+  group_by(storage, shouldBuy) %>%
+  summarise(n=n()) %>%
+  ggplot(aes(fill = shouldBuy, y = storage, x = n)) + 
+  geom_bar(position = "fill", stat = "identity",  colour = "black", alpha = 0.75) + 
+  scale_fill_viridis(discrete = T) +
+  scale_x_continuous(labels = scales::percent) +
+  labs(x = "", y = "",title = "Storage") + 
+  theme(legend.position="none")
+
+p6 =  outer_train %>%
+  group_by(doors, shouldBuy) %>%
+  summarise(n=n()) %>%
+  ggplot(aes(fill = shouldBuy, y = doors, x = n)) + 
+  geom_bar(position = "fill", stat = "identity",  colour = "black", alpha = 0.75) + 
+  scale_fill_viridis(discrete = T) +
+  scale_x_continuous(labels = scales::percent) +
+  labs(x = "", y = "",title = "Doors") + 
+  theme(legend.position="none")
+
+(p1 + p2) / (p3 + p4) / (p5 + p6)
